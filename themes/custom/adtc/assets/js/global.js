a99b70ca15a703d1e3156afe5f5d037be5657f9e
Ismael <ismaelguedessilva@gmail.com>
2024-08-22 20:01:54 -0300

    add flipText

--- themes/custom/adtc/assets/js/global.js
+++ themes/custom/adtc/assets/js/global.js
@@ -3,9 +3,7 @@
 
   Drupal.behaviors.default = {
     attach: function (context, settings) {
-      // PAGINA ONDE ENCONTRAR
-
-
+      // Inicializa o carousel Slick
       $(".slick-carousel").slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -16,7 +14,8 @@
         fade: true,
         cssEase: 'linear'
       });
-      //eventos do slide home
+
+      // Controles manuais do Slick
       $(".slick-prev").on("click", function () {
         $(".slick-carousel").slick("slickPrev");
       });
@@ -24,9 +23,27 @@
       $(".slick-next").on("click", function () {
         $(".slick-carousel").slick("slickNext");
       });
-      
 
-  
-    },
+      // Animação de flip de texto
+      var items = $('.text-flip-item', context);
+      var currentIndex = 0;
+
+      // Adiciona a classe active ao primeiro item após a página carregar
+      items.eq(currentIndex).addClass('active');
+
+      function flipText() {
+        // Remove a classe ativa do texto atual
+        items.eq(currentIndex).removeClass('active');
+
+        // Incrementa o índice
+        currentIndex = (currentIndex + 1) % items.length;
+
+        // Adiciona a classe ativa ao próximo texto
+        items.eq(currentIndex).addClass('active');
+      }
+
+      // Inicia o efeito de rolagem a cada 3 segundos
+      setInterval(flipText, 2000);
+    }
   };
 })(jQuery, Drupal);
