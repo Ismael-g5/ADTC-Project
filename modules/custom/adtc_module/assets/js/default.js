2a9e205977c59af3d85e3b33b2db030be141747f
Ismael <ismaelguedessilva@gmail.com>
2024-09-18 13:09:31 -0300

    adições options

--- modules/custom/adtc_module/assets/js/default.js
+++ modules/custom/adtc_module/assets/js/default.js
@@ -1,17 +1,37 @@
 (function ($) {
-  $.ajax({
-    url: 'http://localhost/Site-ADTC/api/v1/membros/all',
-    method: 'GET',
-    success: function(response) {
-      // Suponha que você queira filtrar no lado do cliente
-      var filteredData = response.filter(function(item) {
-        return item.nome_do_membro === 'Ismael'; // Filtro de exemplo
-      });
 
-      console.log('Filtered Data:', filteredData);
-    },
-    error: function(xhr, status, error) {
-      console.log('Erro:', status, error);
-    }
+  $('#addressSelect').change(function () {
+    var filterString = $(this).val();
+    var [address, city] = filterString.split('-');
+    
+    $.ajax({
+      url: 'http://localhost/Site-ADTC/api/v1/membros/all',
+      method: 'GET',
+      success: function (response) {
+        // Filtra os dados com base no address e city extraídos
+        var filteredData = response.filter(function (item) {
+          return item.endereco.address === address && item.endereco.city === city;
+        });
+
+        var $congregationSelect = $('#congregation'); // Selecione o select onde as opções serão adicionadas
+        $congregationSelect.empty(); // Limpe as opções existentes
+
+        if (filteredData.length > 0) {
+          // Adicione uma opção padrão
+          //$congregationSelect.append($('<option>').text('Select Congregation').val(''));
+          
+          // Adicione uma opção para cada nome_da_congregacao
+          filteredData.forEach(function (item) {
+            $congregationSelect.append($('<option>').text(item.nome_da_congregacao).val(item.nome_da_congregacao));
+          });
+        } else {
+          console.log('No matching data found.');
+        }
+      },
+      error: function (xhr, status, error) {
+        console.log('Erro:', status, error);
+      }
+    });
   });
+
 })(jQuery);
